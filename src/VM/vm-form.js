a5aa3f9df05ef04ac43b72d12b5104e634c97525
Steph-mss <s.simonis@ecole-ipssi.net>
2024-12-06 16:07:21 +0100

    Ansible maj

--- src/VM/vm-form.js
+++ src/VM/vm-form.js
@@ -3,14 +3,12 @@ import React from 'react';
 function VMForm({ os, userName, setUserName, userPassword, setUserPassword, handleCreateVm, loading }) {
   return (
     <>
-      {os.startsWith('Windows') && (
         <div>
           <h3>Nom d'utilisateur (Windows) :</h3>
           <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="Nom d'utilisateur" disabled={loading} />
           <h3>Mot de passe (Windows) :</h3>
           <input type="password" value={userPassword} onChange={(e) => setUserPassword(e.target.value)} placeholder="Mot de passe" disabled={loading} />
         </div>
-      )}
       <button className="btn primary" onClick={handleCreateVm} disabled={loading}>
         {loading ? 'Création en cours...' : 'Créer une VM'}
       </button>
